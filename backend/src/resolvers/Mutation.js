10bac86194fb59abd31fd7b59637539b430d5146
bogdan <bogdanbobletec@gmail.com>
2018-10-16 17:50:30 -0400

    add delete and update options

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -13,6 +13,22 @@ const Mutations = {
 
   return item;
 },
+  updateItem(parent, args, ctx, info) {
+    const updates = { ...args };
+    delete updates.id;
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, info);
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    const item = await ctx.db.query.item({ where }, `{ id title}`);
+    // TODO: check if they have permission
+    return ctx.db.mutation.deleteItem({ where }, info);
+  }
 };
 
 module.exports = Mutations;
---
+++ frontend/pages/update.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import UpdateItem from '../components/UpdateItem';
+const Update = ({ query }) => {
+  return (
+    <div>
+      <UpdateItem id={query.id}/>
+    </div>
+  );
+};
+
+export default Update;
