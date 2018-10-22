0f53c49aabad03ab96a4d53a413dbacb653b6b07
bogdan <bogdanbobletec@gmail.com>
2018-10-21 21:08:35 -0400

    add permissions page

---
+++ frontend/components/Permissions.js
@@ -0,0 +1,131 @@
+import { Query, Mutation } from 'react-apollo';
+import Error from './ErrorMessage';
+import SickButton from './styles/SickButton';
+import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
+import Table from './styles/Table';
+
+const possiblePermissions = [
+  'ADMIN',
+  'USER',
+  'ITEMCREATE',
+  'ITEMUPDATE',
+  'ITEMDELETE',
+  'PERMISSIONUPDATE'
+];
+
+const ALL_USERS_QUERY = gql`
+  query ALL_USERS_QUERY {
+    users {
+      id
+      email
+      name
+      permissions
+    }
+  }
+`;
+
+const UPDATE_PERMISSIONS_MUTATION = gql`
+mutation UPDATE_PERMISSIONS_MUTATION($permissions: [Permission], $userId: ID!) {
+  updatePermissions(permissions: $permissions, userId: $userId) {
+    id
+    permissions
+    name
+    email
+  }
+}
+`;
+
+const Permissions = props => (
+  <Query query={ALL_USERS_QUERY}>
+    {({ data, loading, error }) => (
+      <div>
+        <Error error={error} />
+        <div>
+          <h2>Manage Permissions</h2>
+          <Table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                {possiblePermissions.map(permission => <th key={permission}>{permission}</th>)}
+                <th>Update</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.users.map(user => <UserPermissions user={user} key={user.id}/> )}
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    )}
+  </Query>
+);
+
+class UserPermissions extends React.Component {
+  static propTypes = {
+    user: PropTypes.shape({
+      name: PropTypes.string,
+      email: PropTypes.string,
+      id: PropTypes.string,
+      permissions: PropTypes.arrayOf(PropTypes.string),
+    }).isRequired
+  };
+  state = {
+    permissions: this.props.user.permissions,
+  };
+  handlePermissionChange = e => {
+    const checkBox = e.target;
+    let updatedPermissions = [...this.state.permissions];
+    if (checkBox.checked) {
+      updatedPermissions.push(checkBox.value);
+    } else {
+      updatedPermissions = updatedPermissions.filter(permission => permission !== checkBox.value);
+    }
+    this.setState({ permissions: updatedPermissions})
+  };
+  render() {
+    const user = this.props.user
+    return (
+      <Mutation
+        mutation={UPDATE_PERMISSIONS_MUTATION}
+        variables={{
+          permissions: this.state.permissions,
+          userId: this.props.user.id
+        }}
+      >
+        {(updatePermissions, { loading, error }) => (
+          <React.Fragment>
+            {error && <tr><td colspan="9"><Error error={error} /></td></tr>}
+            <tr>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              {possiblePermissions.map(permission => (
+                <td key={permission}>
+                  <label htmlFor={`${user.id}-permission-${permission}`}>
+                    <input
+                      id={`${user.id}-permission-${permission}`}
+                      type="checkbox"
+                      checked={this.state.permissions.includes(permission)}
+                      value={permission}
+                      onChange={this.handlePermissionChange}
+                    />
+                  </label>
+                </td>
+              ))}
+              <td>
+                 <SickButton
+                   type="button"
+                   disabled={loading}
+                   onClick={updatePermissions}
+                 >Updat{loading ? 'ing' : 'e'}</SickButton>
+              </td>
+            </tr>
+          </React.Fragment>
+          )}
+        </Mutation>
+    )
+  }
+}
+
+export default Permissions;
--- frontend/components/styles/Table.js
+++ frontend/components/styles/Table.js
@@ -10,8 +10,8 @@ const Table = styled.table`
   td,
   th {
     border-bottom: 1px solid ${props => props.theme.offWhite};
+    padding: 5px;
     border-right: 1px solid ${props => props.theme.offWhite};
-    padding: 10px 5px;
     position: relative;
     &:last-child {
       border-right: none;
@@ -19,6 +19,10 @@ const Table = styled.table`
       button {
         width: 100%;
       }
+    }
+    label {
+      display: block;
+      padding: 10px 5px;
     }
   }
   tr {
