8bab5e234310620598debf322b8b1513c734a54b
bogdan <bogdanbobletec@gmail.com>
2018-10-26 12:57:47 -0400

    add logic to add or remove items from cart

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -5,6 +5,7 @@ import Signout from './Signout';
 import { Fragment } from 'react';
 import { Mutation } from 'react-apollo';
 import { TOGGLE_CART_MUTATION } from './Cart';
+import CartCount from './CartCount';
 
 const Nav = () => (
   <User>
@@ -27,7 +28,9 @@ const Nav = () => (
             <Signout />
             <Mutation mutation={TOGGLE_CART_MUTATION}>
               { toggleCart => (
-                <button onClick={toggleCart}>My Cart</button>
+                <button onClick={toggleCart}>My Cart
+                  <CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)} />
+                </button>
               )}
             </Mutation>
           </Fragment>
