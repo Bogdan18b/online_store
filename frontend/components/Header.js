0171f87439d69287702a5bb365a0a766bbaee6b1
bogdan <bogdanbobletec@gmail.com>
2018-10-26 08:38:52 -0400

    add cart

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import styled from 'styled-components';
 import Nav from './Nav';
+import Cart from './Cart';
 import Link from 'next/link';
 import Router from 'next/router';
 import NProgress from 'nprogress';
@@ -69,7 +69,7 @@ const Header = () => (
     <div className="sub-bar">
       <p>Search</p>
     </div>
-    <div>Cart</div>
+    <Cart />
   </StyledHeader>
 );
 
