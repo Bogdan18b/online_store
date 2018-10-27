2506ba1b4e454c3165d2ac88564c917abf00b80f
bogdan <bogdanbobletec@gmail.com>
2018-10-27 00:24:20 -0400

    set up for heroku

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,7 +30,7 @@ server.express.use(async (req, res, next) => {
 server.start({
   cors: {
     credentials: true,
-    origin: process.env.FRONTEND_URL
+    origin: "*"
   }
 }, deets => {
   console.log(`server is now running on port http://localhost:${deets.port}`);
