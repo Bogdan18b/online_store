1514cedeb846c3fd923d1e898f50d9b8f1dfbc43
bogdan <bogdanbobletec@gmail.com>
2018-11-02 11:56:06 -0400

    remove loading messages

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -42,7 +42,7 @@ class Items extends Component {
           }}
         >
           {({ data, error, loading }) => {
-            if (loading) return <p>Loading...</p>;
+            if (loading) return <p>loading...</p>;
             if (error) return <p>Error: {error.message}</p>;
             return (
               <ItemsList>
